6fdaf4a931f98b302675a7d0f541fa88acf4d0ae
Benjamin Bown <benbmb99@gmail.com>
2024-05-18 19:20:15 -0600

    Complete mostly functional app for lists.

--- app/src/main/java/com/hikerhelper/bushwack/List.kt
+++ app/src/main/java/com/hikerhelper/bushwack/List.kt
@@ -1,10 +1,16 @@
 package com.hikerhelper.bushwack
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,26 +22,78 @@ private const val ARG_PARAM2 = "param2"
  * Use the [List.newInstance] factory method to
  * create an instance of this fragment.
  */
-class List : Fragment() {
-    // TODO: Rename and change types of parameters
+class List<T> : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    val button = view?.findViewById<Button>(R.id.addButton) as Button
 
+
+    //Attempts to create a function for the button and set up the list
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        fun OnClickListener(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            // Inflate the fragment layout
+            val rootView: View = inflater.inflate(R.layout.fragment_list, container, false)
+            // Find views by their IDs
+            val textViewExample: TextView = rootView.findViewById(R.id.editItemName)
+            val buttonExample: Button = rootView.findViewById(R.id.addButton)
+            // Set onClickListener for the button
+            buttonExample.setOnClickListener { // Handle button click
+                textViewExample.setText("Button Clicked!")
+            }
+            return rootView
+        }
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
+
+//            // Inflate the layout for the fragment
+//            val view = inflater.inflate(R.layout.fragment_list, container, false)
+//
+//            // Find the button
+//            val button = view.findViewById<Button>(R.id.addButton)
+//            var userList = view.findViewById<ListView>(R.id.itemList)
+//            var texty = view.findViewById<EditText>(R.id.editItemName)
+//            lateinit var actualList: ArrayList<String>
+//            actualList.add("Empty?")
+//
+//            val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(
+//                view.context,
+//                android.R.layout.simple_list_item_checked,
+//                actualList.toTypedArray()
+//            )
+//
+//        userList.adapter = adapter
+//
+//            // Create an OnClickListener object
+//            val listener = View.OnClickListener {
+//                val item = texty.text.toString()
+//
+//                // on below line we are checking if item is not empty
+//                if (item.isNotEmpty()) {
+//                    // on below line we are adding item to our list.
+//                    actualList.add(item)
+//
+//                    // on below line we are notifying adapter
+//                    // that data in list is updated to update our list view.
+//                    adapter.notifyDataSetChanged()
+//                }
+//            }
+//
+//            // Set the OnClickListener on the button
+//            button.setOnClickListener(listener)
+//
+//            return view
+//    }
+
+
 
     companion object {
         /**
@@ -49,7 +107,7 @@ class List : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            List().apply {
+            List<Any>().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/hikerhelper/bushwack/MainActivity.kt
+++ app/src/main/java/com/hikerhelper/bushwack/MainActivity.kt
@@ -1,64 +1,140 @@
 package com.hikerhelper.bushwack
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.View.VISIBLE
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.RelativeLayout
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.node.getOrAddAdapter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.hikerhelper.bushwack.R.id.*
 import com.hikerhelper.bushwack.ui.theme.BushwackTheme
+import com.hikerhelper.bushwack.R.layout.fragment_list
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    //Initialize our variables
     private lateinit var drawerLayout: DrawerLayout
+    /*private lateinit var userList: ListView
+    private lateinit var addButtony: Button
+    private lateinit var editText: EditText
+    private lateinit var listy: ArrayList<String>*/
+    private lateinit var linLayli: LinearLayout
+    private lateinit var relLayli: RelativeLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
+        //Sets up the full layout, including fragments, and activates it
+        drawerLayout = findViewById(drawer_layout)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        //linLayli = findViewById<LinearLayout>(linLay)
+        /*relLayli = findViewById<RelativeLayout>(listContainer)
+        userList = findViewById(itemList)
+        addButtony = findViewById(addButton)
+        editText = findViewById(editItemName)
+        listy = ArrayList()*/
+
+        //Sets up the toolbar above and activates it
+        val toolbar = findViewById<Toolbar>(toolbar)
         setSupportActionBar(toolbar)
 
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        //sets up the sidemenu layout and activates it
+        val navigationView = findViewById<NavigationView>(nav_view)
         navigationView.setNavigationItemSelectedListener (this)
 
+        //Creates the way to open the side menu
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        /*val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this@MainActivity,
+            android.R.layout.simple_list_item_checked,
+            listy.toTypedArray()
+        )
+
+        userList.adapter = adapter
+
+        addButtony.setOnClickListener {
+            val item = editText.text.toString()
+            if(item.isNotEmpty()){
+                listy.add(item)
+                adapter.notifyDataSetChanged()
+            }
+        }*/
+
+        //Checks to see if there is a saved instance of the app already running
         if (savedInstanceState == null){
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_cont, Home()).commit()
-            navigationView.setCheckedItem(R.id.nav_home)
+                .replace(fragment_cont, Home()).commit()
+            navigationView.setCheckedItem(nav_home)
         }
     }
 
+    //(UNUSED) Makes the Home page visible.
+    fun homeVisible(){
+        linLayli.setVisibility(View.VISIBLE)
+        relLayli.setVisibility(View.GONE)
+    }
+
+    //(UNUSED) Makes the List page visible.
+    fun listVisible(){
+        linLayli.setVisibility(View.GONE)
+        relLayli.setVisibility(View.VISIBLE)
+    }
+
+    //Controls the menu
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when(p0.itemId){
-            R.id.nav_home -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_cont, Home()).commit()
-            R.id.nav_list -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_cont, List()).commit()
+            nav_home -> //homeVisible()
+                supportFragmentManager.beginTransaction()
+                .replace(fragment_cont, Home()).commit()
+            nav_list -> //listVisible()
+                supportFragmentManager.beginTransaction()
+                .replace(fragment_cont, List<Any>()).commit()
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
-    @SuppressLint("MissingSuperCall")
+    //Overrides the onClick option for the button
+//    override fun onClick(view: View?) {
+//        val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(
+//            this@MainActivity,
+//            android.R.layout.simple_list_item_checked,
+//            list as ArrayList<String?>
+//        )
+//
+//        val  texty: EditText = findViewById(R.id.editItemName)
+//        val item = texty.text.toString()
+//        println(texty.text.toString())
+//        if (item.isNotEmpty()) {
+//            list.add(item)
+//        }
+
+
+    //}
+
+    //Controls what happens when the back button is pressed
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)){
             drawerLayout.closeDrawer(GravityCompat.START)
@@ -68,6 +144,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 }
 
+//UNUSED FUNCTIONS THAT MAY COME IN HANDY IN THE FUTURE
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -80,6 +157,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BushwackTheme {
-        Greeting("Android")
+        Greeting("name")
     }
 }
